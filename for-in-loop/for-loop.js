b43b43b9f786021131fbf256b22d75241500ab8a
sahil6699 <sahilaneja66199@gmail.com>
2023-01-25 16:20:31 +0530

    for in loop

---
+++ for-in-loop/for-loop.js
@@ -0,0 +1,14 @@
+const object = {
+  fname: 'sahil',
+  lname: 'aneja',
+  age: 24,
+  height: '5"11',
+  weight: 70,
+};
+
+// for in loop
+// can be used to iterate over the prop of a object
+// it also iterate over the the values of the object by using the following syntax: object[objectProperty]
+for (let objectProperty in object) {
+  console.log(`${objectProperty}: ${object[objectProperty]}`);
+}
